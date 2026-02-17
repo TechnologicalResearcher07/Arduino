beea9b021758e6d5ce89a5a3868947c6841eed17
Kiera McKimmy <investedcomputerexplorer07@gmail.com>
2026-02-17 14:12:20 -0500

    Added Developer and Date

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,6 +1,10 @@
 /*
   Blink
 
+Developer: Kiera McKimmy
+
+Date: 17 February 2026
+
   Turns an LED on for one second, then off for one second, repeatedly.
 
   Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
